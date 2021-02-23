22553645f98e7491e42a27be60936d9c519a94ce
Ivan Pozderac <ivan.pozderac@gmail.com>
2021-02-23 02:42:09 +0000

    trying out auth 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,55 @@
-import axios from "axios";
+
+import { AxiosResponse } from "axios";
 import { useEffect } from "react";
-// import { useState } from "react";
-// import NetworkService from "./network/NetworkService";
-// import { getLatestActiveSubreddits } from "./network/Requests";
+import { useState } from "react";
+import NetworkService from "./network/NetworkService";
+import { getLatestActiveSubreddits } from "./network/Requests";
 
 const App:React.FC<{}> = () => {
-  // const networkService = new NetworkService()
+  const networkService = new NetworkService()
 
 
-  // const [latestSubreddits, setLatestSubreddits] = useState({kind: 'none', data: { dist: 0}})
+  const [latestSubreddits, setLatestSubreddits] = useState({kind: 'none', data: { dist: 0}})
+  const [token, setToken] = useState('')
     
-    // const fetchLatestActiveSubreddits = () => {
-    //     const config = getLatestActiveSubreddits()
-    //     networkService.makeRequest(config)
-    //         .then((response: AxiosResponse<any>) => {
-    //             const results = response.data;
-    //             console.log(results);
-    //             setLatestSubreddits(results)
-    //         })
-    // }
+    const fetchLatestActiveSubreddits = (token: string) => {
+        const config = getLatestActiveSubreddits(token)
+        networkService.makeRequest(config)
+            .then((response: AxiosResponse<any>) => {
+                const results = response.data;
+                console.log(results);
+                setLatestSubreddits(results)
+            })
+    }
+
+    useEffect(()=> {
+      getRedditToken()
+    }, [])
 
     useEffect(() => {
-       // fetchLatestActiveSubreddits();
-        aToken()
-    })
+      fetchLatestActiveSubreddits(token);
+    },[token])
 
-    const aToken = () => (
-      axios
-    .post(
-      "https://www.reddit.com/api/v1/access_token?grant_type=client_credentials",
-      '',
-      {
-        headers: {
-          Authorization: `Basic ${Buffer.from(
-            `${process.env.REDDIT_CLIENT_ID}:${process.env.REDDIT_CLIENT_SECRET}`
-          ).toString("base64")}`,
-          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-        }
-      }
-    )
-    .then((r) => {console.log(r)})
 
-    )
+
+    async function getRedditToken() {
+      const  REDDIT_CLIENT_ID  = 'Xn9gIG6QLg0O3Q';
+      const REDDIT_CLIENT_SECRET = 'AcmUZTtO-dGSj7qtpTN52ejNzhXs7A';
+      const REDDIT_ACCESS_TOKEN_URL = 'https://www.reddit.com/api/v1/access_token';
+  
+      const token = await fetch(REDDIT_ACCESS_TOKEN_URL, {
+         method: 'POST',
+          body: 'grant_type=client_credentials',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Basic ` + Buffer.from(`${REDDIT_CLIENT_ID}:${REDDIT_CLIENT_SECRET}`).toString('base64') // Not sure about this
+          }
+      }).then(function(response) {
+          console.log(response);
+          return response.json();
+      })
+      setToken(token.access_token);
+  }
   
 
   return (
@@ -50,7 +58,7 @@ const App:React.FC<{}> = () => {
        Search - extra
       </header>
       <main>
-      
+      {latestSubreddits?.kind}
       </main>
     </>
 )}
--- src/network/Requests.ts
+++ src/network/Requests.ts
@@ -4,12 +4,15 @@ import Config from './Config'
 const maxResults = 10
 // const key = Config.authorization
 
-export const getLatestActiveSubreddits = () => {      
+export const getLatestActiveSubreddits = (token: string) => {      
     return {
         method: appConstants.apiMethods.GET,
         url: `${Config.baseUrl}/subreddits.json`,
         params: {
             limit: maxResults
+        },
+        headers: {
+            Authorization: `Bearer ${token}`
         }
     }
 }
